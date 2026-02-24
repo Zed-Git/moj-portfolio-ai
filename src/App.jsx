9bb75adbacbc56d878747ef40716d7a1fe4a6b85
Zdravko Mijailovic <mdzdravko@gmail.com>
2026-02-23 16:32:39 -0800

    DODATNE Popravke,zbog znakova i gresaka u VSC Exploreru, ALI vracanje..motion u App.jsx...

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { motion } from 'framer-motion'; 
+// eslint-disable-next-line
+import { motion } from 'framer-motion';
 import Header from './components/Header';
 import About from './sections/About';
 import Projects from './sections/Projects';
